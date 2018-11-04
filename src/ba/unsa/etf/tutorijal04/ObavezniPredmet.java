1da7af4ef6161eb8c3f2692088e0706396af36bd
ebejtovic1 <ebejtovic1@etf.unsa.ba>
2018-11-04 18:03:22 +0100

    Dodan kostur klasa IzborniPredmet i ObavezniPredmet

---
+++ src/ba/unsa/etf/tutorijal04/ObavezniPredmet.java
@@ -0,0 +1,40 @@
+package ba.unsa.etf.tutorijal04;
+
+public class ObavezniPredmet extends Predmet {
+    private String nazivPredmeta="";
+    private int ETCSbodovi;
+    private int brojStudenata;
+    private final int maxBr;
+    private Student [] upisaniStudenti=null;
+
+    public String getNazivPredmeta ( ) {
+        return nazivPredmeta;
+    }
+
+    public void setNazivPredmeta (String nazivPredmeta) {
+        this.nazivPredmeta = nazivPredmeta;
+    }
+
+    public int getETCSbodovi ( ) {
+        return ETCSbodovi;
+    }
+
+    public void setETCSbodovi (int ETCSbodovi) {
+        this.ETCSbodovi = ETCSbodovi;
+    }
+
+    public ObavezniPredmet (String nazivPredmeta, int ETCSbodovi, int maxBr) {
+        this.nazivPredmeta = nazivPredmeta;
+        this.ETCSbodovi = ETCSbodovi;
+        this.brojStudenata = 0;
+        this.maxBr = maxBr;
+        upisaniStudenti=new Student[50];
+    }
+    public void upisiStudenta(Student s){
+       return;
+    }
+    @Override
+    public String ispisi(){
+        return null;
+    }
+}
