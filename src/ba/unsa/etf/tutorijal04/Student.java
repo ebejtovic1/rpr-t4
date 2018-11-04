82c4ab80f93611922fdccf57a29968236c2a84a1
ebejtovic1 <ebejtovic1@etf.unsa.ba>
2018-11-04 16:41:06 +0100

    Dodan kostur klase Student

---
+++ src/ba/unsa/etf/tutorijal04/Student.java
@@ -0,0 +1,16 @@
+package ba.unsa.etf.tutorijal04;
+
+public class Student {
+    private String imeIPrezime="";
+    private int brIndexa=0;
+
+    public Student(String a, int index){
+        imeIPrezime=a;
+        brIndexa=index;
+    }
+    //geteri i seteri
+
+    public String Ispisi(){
+        return null;
+    }
+}
